7f39d94549d30d56d07714c942f5aba496154c77
Re1d0n <reiduninnes@gmail.com>
2022-03-22 17:56:33 -0700

    added timer

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("test")
+function startTimer(duration, display) {
+    var timer = duration, seconds;
+    setInterval(function () {
+        
+        seconds = parseInt(timer % 50, 10);
+
+        
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 50 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
